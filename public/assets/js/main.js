95614753a81e9b83dff764a87b66df57f5f169bb
Michał Suleja <michalsuleja@gmail.com>
2022-01-19 11:58:21 +0100

    desktop view

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,8 +1,26 @@
-const burger = document.querySelector('.burger-icon');
+const burger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu__burger');
 
 const handleMenu = () => {
     menu.classList.toggle('menu-active');
+    burger.classList.toggle('is-active');
 }
 
 burger.addEventListener('click', handleMenu);
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
