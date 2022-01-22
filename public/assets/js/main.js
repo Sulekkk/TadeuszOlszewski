d3388ad001dfc4780a53aff5f6ee04df6608cb25
Michał Suleja <michalsuleja@gmail.com>
2022-01-22 12:28:20 +0100

    feat: new features added to burger menu. Section projects shortened

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,12 +1,23 @@
 const burger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu__burger');
+const menuItems = document.querySelectorAll('.menu-item')
 
 const handleMenu = () => {
     menu.classList.toggle('menu-active');
     burger.classList.toggle('is-active');
+    document.body.classList.toggle('stop-scroll');
+
+    menuItems.forEach(menuItem =>{
+      menuItem.addEventListener('click', ()=>{
+        menu.classList.remove('menu-active');
+        burger.classList.remove('is-active');
+        document.body.classList.remove('stop-scroll');
+      })
+    })
 }
 
 burger.addEventListener('click', handleMenu);
+
 
 const swiper = new Swiper('.swiper', {
     // Optional parameters
@@ -27,3 +38,5 @@ const swiper = new Swiper('.swiper', {
       delay: 10000,
     },
   });
+
+  console.log(menuItems);
